294ab911989c434d136b94f4c203bf8720b9b983
F Cesar Farias <fcesarf80@gmail.com>
2025-06-10 16:38:00 +0100

    var 2
    continue

--- comparacao.cpp
+++ comparacao.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    // const int a = 5;
-    // const auto flo_a = static_cast<float>(a);
-	// const float b = 4.9;
-	// const auto int_b = static_cast<int>(b);//conversao C++
-	// cout<<"O valor de a convertido para float eh: "<<flo_a<<endl;
-    // cout<<"O valor de b convertido para inteiro eh: "<<int_b<<endl;
+    int a = 5;
+    float b = 4.9;
+    float resul_a = static_cast<float>(a);
+    int resul_b = static_cast<int>(b);
+    
+    float resul_c = a + b;// Operação sem conversão    
+    int resul_d = static_cast<int>(resul_a);//Conversão explícita
+    cout<<"O valor da variavel 'a' do tip inteiro eh: 5"<<endl;
+    cout<<"O valor da variavel 'b' do tip float eh: 4.9"<<endl;
+    cout<<"O valor de 'a' convertido para float eh: "<<resul_a<<endl;
+    cout<<"O valor de 'b' convertido para inteiro eh: "<<resul_b<<endl;
+    cout<<"A soma das variaveis 'a' e 'b' eh: "<<resul_c<<endl;
+    cout<<"A soma das variaveis 'a' e 'b' convertida para inteiro eh: "<<resul_d<<endl;
 
-    int x=16, y=3;
-    int result= x/y;
-    cout<<"Resultado int= "<<result<<endl; 
+    // int x=16, y=3;
+    // int result= x/y;
+    // cout<<"Resultado int= "<<result<<endl; 
 
-    float r=(float)x/y;
-    cout<<"Resultado float= "<<r<<endl;
+    // float r=(float)x/y;
+    // cout<<"Resultado float= "<<r<<endl;
 
-    //C++ static_cast
-    char c='a';
-    int i=static_cast<int>(c);
-    cout<<"i = "<<i<<endl;
+    // //C++ static_cast
+    // char c='a';
+    // int i=static_cast<int>(c);
+    // cout<<"i = "<<i<<endl;
 
     return 0;
 }
