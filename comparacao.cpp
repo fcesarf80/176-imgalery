e300b21c65a4b1e5f9211730538e17e2ee3dffe9
F Cesar Farias <fcesarf80@gmail.com>
2025-06-11 15:58:55 +0100

    conversão implicita

--- comparacao.cpp
+++ comparacao.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int a = 5;
-    float b = 4.9;
-    float resul_a = static_cast<float>(a);
-    int resul_b = static_cast<int>(b);
+int main ()
+{
+    //ex m3.6
+    int n=5;
+    float x=1.2;
+    int xInt = (int) x;
+    cout<<"x="<<x<<endl;
+    cout<<"xInt="<<xInt<<endl;
     
-    float resul_c = a + b;// Operação sem conversão    
-    int resul_d = static_cast<int>(resul_a);//Conversão explícita
-    cout<<"O valor da variavel 'a' do tip inteiro eh: 5"<<endl;
-    cout<<"O valor da variavel 'b' do tip float eh: 4.9"<<endl;
-    cout<<"O valor de 'a' convertido para float eh: "<<resul_a<<endl;
-    cout<<"O valor de 'b' convertido para inteiro eh: "<<resul_b<<endl;
-    cout<<"A soma das variaveis 'a' e 'b' eh: "<<resul_c<<endl;
-    cout<<"A soma das variaveis 'a' e 'b' convertida para inteiro eh: "<<resul_d<<endl;
+    return 0;
+}
+
+// int main() {
+//     int a = 5;
+//     float b = 4.9;
+//     float resul_a = static_cast<float>(a);
+//     int resul_b = static_cast<int>(b);
+    
+//     float resul_c = a + b;// Operação sem conversão    
+//     int resul_d = static_cast<int>(resul_a);//Conversão explícita
+//     cout<<"O valor da variavel 'a' do tip inteiro eh: 5"<<endl;
+//     cout<<"O valor da variavel 'b' do tip float eh: 4.9"<<endl;
+//     cout<<"O valor de 'a' convertido para float eh: "<<resul_a<<endl;
+//     cout<<"O valor de 'b' convertido para inteiro eh: "<<resul_b<<endl;
+//     cout<<"A soma das variaveis 'a' e 'b' eh: "<<resul_c<<endl;
+//     cout<<"A soma das variaveis 'a' e 'b' convertida para inteiro eh: "<<resul_d<<endl;
 
     // int x=16, y=3;
     // int result= x/y;
@@ -28,8 +39,8 @@ int main() {
     // int i=static_cast<int>(c);
     // cout<<"i = "<<i<<endl;
 
-    return 0;
-}
+    // return 0;
+// }
 
 
 //Em C++, "cast" é traduzido para conversão de tipo ou conversão explícita. Refere-se à alteração do tipo de um objeto ou valor de um tipo para outro..
