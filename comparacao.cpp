92f224833454bd5db9e01577bb6612374b1a0fbd
F Cesar Farias <fcesarf80@gmail.com>
2025-06-19 18:02:50 +0100

    setlocat LC_ALL

--- comparacao.cpp
+++ comparacao.cpp
@@ -640,37 +640,113 @@ antes da divisão e mostre o novo resultado.
  }
 */
 
-/*ex.3_Crie um programa que:
-Solicite ao utilizadpr que digite um ùnico caractere.
-Converta esse caractere (char)para seu valor numérico na tela ASCII.
-Exiba o valor inteiro correspondente ao caractere digitado.
+// ex.3_Crie um programa que:
+// Solicite ao utilizadpr que digite um ùnico caractere.
+// Converta esse caractere (char) para seu valor numérico na tela ASCII.
+// Exiba o valor inteiro correspondente ao caractere digitado.
 
+//     #include <iostream>
+//     #include <iomanip>
+
+//     int main ()
+//     {
+//         setlocale (LC_ALL, "portuguese");
+//         int T1, AsciiTecla = (int) T1;
+//         char Reply;
+
+//         do
+//         {
+//             int AsciiTecla = T1;
+//             std::cout << "Digite uma tecla: "<<std::endl;
+//             std::cin >> T1;
+//             std::cout << "\nTecla em ASCII: " << AsciiTecla <<std::endl;
+
+//             std::cout << "\n" <<"Deseja verificar outra tecla? (S) ou (N): ";
+// 		    std::cin >> Reply;
+
+//         } while (Reply == 'S' || Reply == 's');
+
+//         std::cout << "\n" << "Saindo do programa..."<< std::endl;
+//         system("Pause");
+
+//         return 0;
+    
+// }
+
+//    *******************************************************************************************
+/*
     #include <iostream>
-    using namespace std;
+    #include <iomanip>
+
     int main ()
     {
-    char tecla='a';
-    int AsciiTecla= (int) tecla;
-    cout<<"Tecla em ASCII= "<< AsciiTecla<<endl;
-    return 0;
+        setlocale (LC_ALL, "portuguese");
+        int a;
+        float b;
+        char Reply;
+
+        do
+        {
+            std::cout << "Digite um número inteiro: "<<std::endl;
+            std::cin >> a;
+            std::cout << "Digite um número décimal: "<<std::endl;
+            std::cin >> b;
+            std::cout<<std::endl;
+
+            float resul_a = static_cast<float>(a);
+            int resul_b = static_cast<int>(b);    
+            float resul_c = a + b;// Operação sem conversão    
+            int resul_d = static_cast<int>(resul_a) + b;//Conversão explícita
+            std::cout<<"O valor da váriavel 'a' do tip inteiro é: "<<a<<"\n";
+            std::cout<<"O valor da váriavel 'b' do tip float é: "<<b<<"\n";
+            std::cout<<"O valor de 'a' convertido para float é: "<<resul_a<<"\n";
+            std::cout<<"O valor de 'b' convertido para inteiro é: "<<resul_b<<"\n";
+            std::cout<<"A soma das váriaveis 'a' e 'b' é: "<<resul_c<<"\n";
+            std::cout<<"A soma das váriaveis 'a' e 'b' convertida para inteiro é: "<<resul_d<<"\n";
+
+            std::cout << "\n" <<"Deseja verificar outras números? (S) ou (N): "<<"\n";
+		    std::cin >> Reply;
+            std::cout<<std::endl;
+        
+        } while (Reply == 'S' || Reply == 's');
+
+        std::cout << "\n" << "Saindo do programa..."<< std::endl;
+        system("Pause");
+        return 0;
+
     }
 */
+//    *******************************************************************************************
 
-/*  int main()
-    {
-    int a = 5;
-    float b = 4.9;
-    float resul_a = static_cast<float>(a);
-    int resul_b = static_cast<int>(b);    
-    float resul_c = a + b;// Operação sem conversão    
-    int resul_d = static_cast<int>(resul_a);//Conversão explícita
-    cout<<"O valor da variavel 'a' do tip inteiro eh: 5"<<endl;
-    cout<<"O valor da variavel 'b' do tip float eh: 4.9"<<endl;
-    cout<<"O valor de 'a' convertido para float eh: "<<resul_a<<endl;
-    cout<<"O valor de 'b' convertido para inteiro eh: "<<resul_b<<endl;
-    cout<<"A soma das variaveis 'a' e 'b' eh: "<<resul_c<<endl;
-    cout<<"A soma das variaveis 'a' e 'b' convertida para inteiro eh: "<<resul_d<<endl;
-*/
+
+/*      int main ()
+
+    // setlocale(LC_ALL, "portuguese");
+    // int vl1, vl2;
+    // char Result; //Reply
+    // do
+    // {
+    //     std::cout << "Insira o 1° valor: ";
+    //     std::cin >> vl1;
+    //     std::cout << "Insira o 2° valor: ";
+    //     std::cin >> vl2;
+    //     std::cout << vl1 << " > " << vl2 <<" : " << (vl1>vl2) <<std::endl; 
+    //     std::cout << vl1 << " < " << vl2 <<" : " << (vl1<vl2) <<std::endl;
+    //     std::cout << vl1 << " >= " << vl2 <<" : " << (vl1>=vl2) <<std::endl;
+    //     std::cout << vl1 << " <= " << vl2 <<" : " << (vl1<=vl2) <<std::endl;
+    //     std::cout << vl1 << " == " << vl2 <<" : " << (vl1==vl2) <<std::endl;
+    //     std::cout << vl1 << " != " << vl2 <<" : " << (vl1!=vl2) <<std::endl;
+
+    //     std::cout << "\n" <<"Deseja verificar outras números? (S) ou (N): ";
+	// 	std::cin >> Result;
+        
+    // } while (Result == 'S' || Result == 's');
+
+    //     std::cout << "\n" << "Saindo do programa..."<< std::endl;
+    //     system("Pause");
+    //     return 0;
+    
+    //                     }*/
 
 /*  int x=16, y=3;
     int result= x/y;
@@ -688,6 +764,8 @@ Exiba o valor inteiro correspondente ao caractere digitado.
 //Em C++, "cast" é traduzido para conversão de tipo ou conversão explícita. Refere-se à alteração do tipo de um objeto ou valor de um tipo para outro..
 
 //Exercício de C++:Faça um programa que pede 2 valores, em seguida exiba operações de comparação.
+
+/*
 #include <iostream>
 #include <iomanip>
 
@@ -719,3 +797,4 @@ int main ()
         return 0;
     
     }
+        */
