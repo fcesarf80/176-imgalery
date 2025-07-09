ef40cdad1170e68da99964060631d3e58e4f018e
F Cesar Farias <fcesarf80@gmail.com>
2025-07-09 11:09:58 +0100

    switch case

--- output/funcoes.cpp
+++ output/funcoes.cpp
@@ -1,5 +1,78 @@
 //PROTOTIPO DE FUNÇÕES - calculadora
 #include<iostream>
+double Soma(char Operacao, double Num1, double Num2);
+
+int main()
+{
+    double Num1, Num2;
+    char Operacao;
+    std::cout << "\nDigite o 1° número: ";
+    std::cin >> Num1;
+    std::cout << "Digite o 2° número: ";
+    std::cin >> Num2;
+    std::cout << "\n + Soma \n - Subtração \n * Multiplicação \n / Divisão: ";
+    std::cout << "\n\n Digite o simbolo da operação desejada: ";
+    std::cin >> Operacao;
+    std::cout << "\nO resultado da operação: " << Soma(Operacao, Num1, Num2);
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+        return 0;
+}
+// Aqui o case vai selecionar a operação e fará o digitado e selecionado pelo usúario
+    double Soma(char Operacao, double Num1, double Num2)
+    {   
+        switch (Operacao)
+        { 
+        case '+': return Num1 + Num2;
+        case '-': return Num1 - Num2;
+        case '*': return Num1 * Num2;
+        case '/': return Num1 / Num2;
+        defaul: std::cout << "Operador invalido!";
+        break;
+    }
+    return 0;
+}
+/*
+//PROTOTIPO DE FUNÇÕES
+// Crie uma função que indique se um número é par ou impar.
+#include <tchar.h> // Biblioteca C++ Pt-Br
+#include<iostream>
+using namespace std;
+//Protótipo da função
+bool isPar(int num);
+
+int main()
+{    
+    int Numero;
+    std::cout << "\nDigite um número inteiro: ";
+    std::cin >> Numero;
+    // if (isPar(Numero) == true)
+      if (isPar(Numero))// profissionalmente só se usa assim
+    {
+        std::cout << "O número: " << Numero<<" é PAR.\n";
+    }//se não for PAR já sabemos que é impar, tbm então removemos o if
+    else
+    {
+        std::cout <<"O número: " << Numero << " é IMPAR.\n";        
+    }
+    system("PAUSE");
+    return 0;
+}
+//se o resto da divisão for 0 o número é par, retorne true
+bool isPar(int num)
+{
+    if(num % 2 == 0)
+        return true;
+//obs que o return faz a saida imediata da função, logo se for par, o código else é desnecessário
+        return false; 
+}
+
+*/
+
+/*
+//PROTOTIPO DE FUNÇÕES - calculadora
+#include<iostream>
 float Soma(float num1, float num2);
 float Subtracao(float num1, float num2);
 float Multiplicacao(float num1, float num2);
@@ -42,44 +115,6 @@ return num1 * num2;
 float Divisao(float num1, float num2)
 {
 return num1 / num2;
-}
-
-
-
-
-/*
-//PROTOTIPO DE FUNÇÕES
-// Crie uma função que indique se um número é par ou impar.
-#include <tchar.h> // Biblioteca C++ Pt-Br
-#include<iostream>
-using namespace std;
-//Protótipo da função
-bool isPar(int num);
-
-int main()
-{    
-    int Numero;
-    std::cout << "\nDigite um número inteiro: ";
-    std::cin >> Numero;
-    // if (isPar(Numero) == true)
-      if (isPar(Numero))// profissionalmente só se usa assim
-    {
-        std::cout << "O número: " << Numero<<" é PAR.\n";
-    }//se não for PAR já sabemos que é impar, tbm então removemos o if
-    else
-    {
-        std::cout <<"O número: " << Numero << " é IMPAR.\n";        
-    }
-    system("PAUSE");
-    return 0;
-}
-//se o resto da divisão for 0 o número é par, retorne true
-bool isPar(int num)
-{
-    if(num % 2 == 0)
-        return true;
-//obs que o return faz a saida imediata da função, logo se for par, o código else é desnecessário
-        return false; 
 }
 
 */
