494250facef0581d7f084108e25a2e941655a4c4
F Cesar Farias <fcesarf80@gmail.com>
2025-07-13 21:28:21 +0100

    Compilation successful!
    ptrg que pode apontar para qualquer lugar

--- output/output/CriandoFuncoes.cpp
+++ output/output/CriandoFuncoes.cpp
@@ -1,7 +1,45 @@
-#include <iostream>		//Variáveis de memória
+#include <iostream>		
 #include <tchar.h> 
 using namespace std;
- 
+int main()
+{	setlocale(LC_ALL, "portuguese");	
+	int Numero{ 4890 };
+	char Letra {'E'};
+	//Declarei ponteiro genérico ou vazio (void)...
+	//Isso significa que ele não tem ainda um tipo que possa apontar.
+	//O que é útil quando você por exemplo, retorna algo de alguma função e atribui, de antemão, que tipo de dado você vai retornar sendo assim, depois você pode especificar qual tipo de dado o ponteiro void* passará a apontar 
+	void *ptrG;
+	//Agora ptrG passa a apontar para a variavel Letra, pois ele recebeu o endereço de memória desta variável 
+	ptrG = &Letra;
+	//Por que deu ERRO?
+	//Lembre-se que o endereço de uma variável é apenas o 1° byte desta variável, o que elea esta carregada.
+	//Assim... como o compilador vao saber como LER a variável
+	//ex.: a partir do endereço 105 eu devo percorrer quantos bytes?
+	//Ele só saberá isso quando você indicar o tipo do ponteiro ptrG
+	//Por enquanto ele é void!
+	//Logo, já que Letra é do tipo char você deve indicar que o ponteiro ptrG está apontando para um char.
+	//Isso é feito via cast
+	//Porque ptrG não sabe
+	std::cout << "\nValor de Letra via ptrG: " << *(char*)ptrG << "\n";
+	std::cout << "endl";
+	//Primeiro você faz o cast e depois coloca o sinal de desreferenciamento
+	//*(cast para tipo desejado)NomePonteiroGenerico
+	//*(char*)ptrG
+	//agora o compilador sabe que deverá ler 1(byte) para colocar um char na tela!
+	system("PAUSE");
+	return 0;
+	// ptrG = &Numero;
+	
+	// std::cout << "Valor de Número via ptrG: " << *(int*)ptrG << "\n";
+	// std::cout << endl;
+	
+	// return 0;
+}
+//**********************************************************
+/*
+#include <iostream>		//Variáveis de memória
+#include <tchar.h> 
+using namespace std; 
 int main()
 {
 	setlocale(LC_ALL, "portuguese");
@@ -20,13 +58,10 @@ int main()
 	//std::cout << ptr
 	std::cout << "\nEndereço de ptr: " << &ptr <<"\n";
 	std::cout << endl;
-	system("pause");
-	
+	system("pause");	
 	return 0;
 }
-
-Armazenando com & e = endereços de outras variáveis dentro de variáveis ponteiro
-
+*/
 //**********************************************************
 /*#
 include <iostream>		//Variáveis de memória
