9ed033ecad50c8cadeaa872d02fe704393e1c50b
F Cesar Farias <fcesarf80@gmail.com>
2025-07-14 21:25:43 +0100

    tomorrow
    Mais ERROS encontrados : (

--- output/output/ArrayVetores.cpp
+++ output/output/ArrayVetores.cpp
@@ -1,29 +1,18 @@
-#include <iostream>
+#include <iostream>     // Soma de 10 Númeos com Array  //
 #include <string>
 
 int main()
-{
-	
-	//Você pode suprimir o número nos colchetes.
-	float Notas[]{ 0.0f, 1.0f, 2.0f, 3.0f, 4.0f }; // se quiser iniciar todos com zero pode ser assim com único zero 0.0
-	float Media, Soma{ 0.0 };
-	std::string NomeAluno;
-
-	std::cout << "\nDigite o nome do Aluno: ";
-	std::cin >> NomeAluno;
-	
-	for (int i = 0; i <= 4; i++)
-	{
-		std::cout << "\nDigite a nota" << (i+1)<< ": ";
-		std::cin >> Notas[i];
-		Soma = Soma + Notas[i];
-		system("CLS");
-	}
-	
-	Media = Soma/5;
-	
-	std::cout << "\nA Media das notas do aluno foi: " << Media << "\n";
-	
+{	
+	double Numeros[10];
+    double Soma{0.0};
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << "Digite o Número" << (i+1) << ": ";
+        std::cin >> Numeros[i];
+        Soma += Numeros[i]; //Soma = Soma + Numeros[i];
+        system("CLS");
+    }
+	std::cout << "\nA Soma dos números é: " << Soma;
 	system("PAUSE");
 	return 0;
 }
